552db03ae736b1462ef62d7cc1da8812037e85f1
Alister A. Fenix <alister.fenix@gmail.com>
2024-08-23 01:31:14 +0000

    Add server directory and update server.js and start script

--- server/server.js
+++ server/server.js
@@ -4,12 +4,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+// TODO: load environment variables from .env file
+require("dotenv").config()
+
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../build')));
 
 // Catch all route to serve index.html for any unmatched routes (supports React Router)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 app.listen(PORT, () => {
