a5651d28296f936382a30cc51d79c735c739591c
Alister A. Fenix <alister.fenix@gmail.com>
2024-08-26 21:37:38 +0000

    Update styling to customize google popups

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -5,25 +5,26 @@ const AboutSection = () => {
   return (
     <div className="background-container">
       <div className='content'>
-      <h1 id='about-header'>What is <br/>Safe Space Share?</h1>
-      <p>
-        We all navigate public spaces in our unique ways, influenced by who we
-        are and how others see us. These experiences shape our sense of safety,
-        well-being, and belonging, yet they often go unseen, especially as a
-        collective.
-      </p>
-      <br/>
-      <p>
-        Safe Space Share (S3) is here to change that. Anonymously share your
-        stories, explore how your identity influences your experiences, and
-        connect with the diverse perspectives of others. With S3's interactive
-        map, you can uncover patterns and similarities across different
-        communities, helping us all gain deeper insights into our shared and
-        diverse worlds.
-      </p>
-      <h2 id='about-subtitle'>
-        Let's get started!
-      </h2>
+      <h1 id='about-header'>Share your experiences,<br /> Shape our understanding.</h1>
+        <p>
+        We all navigate public spaces in our own unique ways, influenced by who we are and how others see us.
+        These experiences shape our sense of safety, well-being, and belonging, yet they often go unseen,
+        especially as a collective.
+        Safe Space Share lets you anonymously share your personal experiences in different places—whether
+        positive or negative—and discover our collective emotional landscape through an interactive map.<br />
+        We are all different, but our feelings are shared. Add your voice to our collective story.
+        </p>
+        {/* <h2 id='about-subtitle'>
+          Let's get started!
+        </h2> */}
+        <div className="navigation-buttons">
+          <button onClick={() => document.getElementById('contribute').scrollIntoView({ behavior: 'smooth' })}>
+            Share Your Experience
+          </button>
+          <button onClick={() => document.getElementById('explore').scrollIntoView({ behavior: 'smooth' })}>
+            Explore Experiences
+          </button>
+        </div>
       </div>
     </div>
   )
